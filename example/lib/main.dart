521e6b496e9a19a1a3fe5d2950326e6ad83a534e
Yegor Blinov <eblinov@mtsbank.ru>
2023-07-20 14:31:19 +0300

    Plugin: feat: Configuration added fix: Unnecessary renders removed (optimized)
    Example app:
    feat: Animations added
    feat: App layout changed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:shake_tune/shake_tune.dart';
 
@@ -17,26 +16,78 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool animationFlag = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: ShakeTune(
-        callback: () {
-          setState(() {});
-        },
-        child: Center(
-          child: SizedBox(
-            height: 100,
-            width: 100,
-            child: ColoredBox(
-              color: Color((Random().nextDouble() * 0xFFFFFF).toInt())
-                  .withOpacity(1.0),
-              child: Text('text'),
-            ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('YOU WON:', style: TextStyle(fontSize: 21.0),),
+               ShakeTune(
+                  probabilities: const [0.2, 0.2, 0.2, 0.2, 0.2],
+                  alwaysChooseDifferentElement: true,
+                  firstElementShownOnlyOnce: true,
+                  callback: () {
+                    setState(() {
+                      animationFlag = !animationFlag;
+                    });
+                  },
+                  children: [
+                    prizeItem('Shake your phone', Icons.edgesensor_high_rounded),
+                    prizeItem('Car', Icons.directions_car_rounded),
+                    prizeItem('House', Icons.house_rounded),
+                    prizeItem('100 ₽ balance top up', Icons.currency_ruble),
+                    prizeItem('Console', Icons.games_rounded),
+                    prizeItem('- 7000 ₽ fire alarm penalty', Icons.fire_extinguisher_rounded),
+                  ]),
+
+            ],
           ),
-        ),
-      )),
+        )),
+    );
+  }
+
+  Widget prizeItem(String text, IconData icon) {
+    return AnimatedSwitcher(
+        duration: const Duration(milliseconds: 1000),
+        transitionBuilder: (Widget child, Animation<double> animation) {
+          return ShakeAnimation(child: child);
+        },
+        layoutBuilder: (Widget? currentChild, List<Widget> previousChildren) {
+          if (currentChild != null) {
+            return currentChild;
+          }
+          return const Text('Error');
+        },
+        child:Column(key: ValueKey<bool>(animationFlag), children: [
+          Icon(icon, color: Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0), size: 250.0),
+          Text(text)
+        ]));
+  }
+}
+
+class ShakeAnimation extends StatelessWidget {
+  final Widget child;
+  final Duration duration;
+
+  const ShakeAnimation({super.key, required this.child, this.duration = const Duration(milliseconds: 500)});
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+      tween: Tween(begin: 0.0, end: 1.0),
+      duration: duration,
+      builder: (context, value, child) {
+        return Transform.translate(
+          offset: Offset(sin(value * pi * 10) * 10, 0.0),
+          child: child,
+        );
+      },
+      child: child,
     );
   }
 }
--- lib/shake_tune.dart
+++ lib/shake_tune.dart
@@ -1,22 +1,140 @@
+import 'dart:math';
+
 import 'package:flutter/widgets.dart';
 import 'package:shake_tune/shake_detector.dart';
 
 class ShakeTune extends StatefulWidget {
-  final Widget child;
-  final VoidCallback callback;
+  final List<Widget> children;
+  final List<double>? probabilities;
+  final bool? firstElementShownOnlyOnce;
+  final bool? alwaysChooseDifferentElement;
+  final VoidCallback? callback;
 
-  const ShakeTune({super.key, required this.child, required this.callback});
+  /// Creates a ShakeTune widget.
+  ///
+  /// On shake randomly chooses one element from the [children] array.
+  ///
+  /// If the [probabilities] array (the length shall be equal to length of the
+  /// [children] array, and the sum of all elements shall be equal to one)
+  /// is not provided probabilities of choosing one element distributed evenly,
+  /// if the [probabilities] array provide widget use chances from probabilities
+  /// array.
+  ///
+  /// The [children] parameter must not be null.
+  ///
+  /// The [callback] called when phone shook.
+  ///
+  /// The [firstElementShownOnlyOnce] property defines if first element shows
+  /// only on first start (before shake).
+  /// The [alwaysChooseDifferentElement] property defines if we should choose
+  /// different element on every shake.
+  const ShakeTune({
+    super.key,
+    required this.children,
+    this.probabilities,
+    this.callback,
+    this.firstElementShownOnlyOnce,
+    this.alwaysChooseDifferentElement,
+  });
 
   @override
   State<ShakeTune> createState() => _ShakeTuneState();
 }
 
 class _ShakeTuneState extends State<ShakeTune> {
+  int indexToShow = 0;
+
+  void validateProbabilities () {
+    if (widget.probabilities != null) {
+      double probabilitiesSum = widget.probabilities!.reduce((value, element) => value + element);
+
+      if (probabilitiesSum.toStringAsFixed(2) != '1.00') {
+        throw Exception('Probabilities array must have sum of its elements equal to 1');
+      }
+
+      if (widget.firstElementShownOnlyOnce == true && widget.probabilities!.length == widget.children.length - 1) {
+        return;
+      } else if (widget.firstElementShownOnlyOnce == true) {
+        throw Exception('Probabilities array must have the same number of elements as children array minus one if showFirstElementOnlyOnce is true');
+      }
+
+      if (widget.probabilities!.length != widget.children.length) {
+        throw Exception('Probabilities array must have the same number of elements as children array');
+      }
+    }
+  }
+
+  int pickOneWithGivenProbability (int maxIndex, List<double> probabilities) {
+    double rand = Random().nextDouble();
+    double cumulativeProbability = 0.0;
+
+    for (int i = 0; i < maxIndex; i++) {
+      cumulativeProbability += probabilities[i];
+      if (rand < cumulativeProbability) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  int pickOneWithFunction (
+    int previousValue,
+    bool alwaysChangeGeneration,
+    Function random,
+    {int depth = 0}
+  ) {
+    if (depth == 20) {
+      throw Exception('Finding new not repeating element stopped, recursion stopped');
+    }
+
+    int generated = random();
+    if (generated == previousValue && alwaysChangeGeneration == true) {
+      return pickOneWithFunction(previousValue, alwaysChangeGeneration, random, depth: depth+1);
+    }
+    return generated;
+  }
+
+  void handleShake () {
+    int lowerBound = widget.firstElementShownOnlyOnce == true ? 1 : 0;
+    int upperBound = widget.firstElementShownOnlyOnce == true ? widget.children.length-1 : widget.children.length;
+
+    if (widget.probabilities != null) {
+      setState(() {
+        indexToShow = pickOneWithFunction(
+          indexToShow,
+          widget.alwaysChooseDifferentElement == null ? false : widget.alwaysChooseDifferentElement!,
+          () {
+            return pickOneWithGivenProbability(upperBound, widget.probabilities!) + lowerBound;
+          }
+        );
+      });
+    } else {
+      setState(() {
+        indexToShow = pickOneWithFunction(
+          indexToShow,
+          widget.alwaysChooseDifferentElement == null ? false : widget.alwaysChooseDifferentElement!,
+          () {
+            return Random().nextInt(upperBound) + lowerBound;
+          }
+        );
+      });
+    }
+
+    if (widget.callback != null) {
+      widget.callback!();
+    }
+  }
+
   late final ShakeDetector shakeDetector;
+
   @override
   void initState() {
-    shakeDetector = ShakeDetector.autoStart(onPhoneShake: widget.callback);
     super.initState();
+
+    validateProbabilities();
+
+    shakeDetector = ShakeDetector.autoStart(onPhoneShake: handleShake);
   }
 
   @override
@@ -27,18 +145,12 @@ class _ShakeTuneState extends State<ShakeTune> {
 
   @override
   void didUpdateWidget(covariant ShakeTune oldWidget) {
-    if (oldWidget.child != widget.child) {
-      setState(() {});
-    }
-    if (oldWidget.callback != widget.callback) {
-      shakeDetector.changeCallback(widget.callback);
-    }
-
+    shakeDetector.changeCallback(handleShake);
     super.didUpdateWidget(oldWidget);
   }
 
   @override
   Widget build(BuildContext context) {
-    return widget.child;
+    return AnimatedSwitcher(duration: Duration(milliseconds: 100), child: widget.children[indexToShow],);
   }
 }
